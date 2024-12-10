02fc9e1d20578974a1196ce595b9c39732049ed8
VThangws <trinhvietthang27@gmail.com>
2024-12-10 22:36:07 +0700

    Feature/new home page (#16)
    * test
    
    * homepage cap nhat render sanpham,xem chi tiet sam pham,con thieu navbar va sidebar dang update,thieu css bo loc san pham de hoan thien
    
    * Removed a modified file from pull request: style.css
    
    * test
    
    * test
    
    * test
    
    * test
    
    * target css rieng trang homepage
    
    * cap nhat navbar
    
    * update navbar
    
    * test
    
    * test
    
    * cap nhat login tren navbar
    
    * cap nhat navbar va login hoan thien
    
    * xu ly hoan thien navbar,hien thong tin user sau khi login
    
    * test
    
    * hien thi bo loc
    
    * hoan chinh bo loc
    
    * hien thi ten user thanh cong
    
    * hien user tren navbar thanh cong
    
    * test
    
    * hien username tren navbar thanh cong,logout thanh cong
    
    * lay style cart tu nhanh Lapdinh
    
    * test
    
    * fix loi anh Bao noi
    
    * fix comment nham cho
    
    * test code moi giohang,sanpham cua Lapdinh
    
    * homepage hoan chinh
    
    * kêu revert file style.css cũng k thèm làm 2
    
    * kêu revert file style.css chứ có kêu revert main.js đâu???
    
    ---------
    
    Co-authored-by: bhbghghbgb <113711814+bhbghghbgb@users.noreply.github.com>
    Co-authored-by: nguyenthanhhung <nguyenthanhhung.dev@gmail.com>

---
+++ home_page/homeMain.js
@@ -0,0 +1,64 @@
+// function hienTrangChiTiet(id) {
+//   const sanPham = timSanPham(id);
+//   // TODO: mo trang chi tiet san pham
+//   window.location.href = `/Product/ChiTietSanPham/ChiTietSanPham.html?id=${id}`;
+// }
+
+function renderItemSanPham(sanPham) {
+  const item = document.createElement("div");
+  item.classList.add("homepage-grid");
+
+  const img = document.createElement("img");
+  img.src = `../images/${sanPham["image-file"]}`;
+  img.className = "homepage-grid-img";
+  item.appendChild(img);
+
+  const content = document.createElement("div");
+  item.appendChild(content);
+  content.style = "flex:auto";
+
+  const id = document.createElement("h10");
+  id.innerText = sanPham["web-scraper-order"];
+  content.appendChild(id);
+
+  const name = document.createElement("h2");
+  name.innerText = sanPham["name"];
+  content.appendChild(name);
+
+  const checkPrice = document.createElement("div");
+  item.appendChild(checkPrice);
+  checkPrice.className = "homepage-checkPrice";
+
+  const price = document.createElement("h5");
+  price.style = "text-decoration: line-through; color: gray;";
+  price.innerText = sanPham["price"];
+  checkPrice.appendChild(price);
+
+  const sale = document.createElement("p");
+  sale.style = "font-weight:700;color: red";
+
+  const priceSale = formatVND(sanPham["price-sale-n"]);
+  sale.innerText = priceSale;
+  checkPrice.appendChild(sale);
+
+  const category = document.createElement("h4");
+  category.style.setProperty("color", "Green");
+  category.innerText = `-Túi ${sanPham["category"]}-`;
+  item.appendChild(category);
+
+  const btn = document.createElement("button");
+  btn.addEventListener("click", () =>
+    hienTrangChiTiet(sanPham["web-scraper-order"])
+  );
+  btn.className = "homepage-button"
+  btn.textContent = "Xem Chi Tiết";
+  item.appendChild(btn);
+  return item;
+}
+
+if (window.dayLaTrangChu) {
+  // goi khi trang web load thanh cong
+  window.addEventListener("load", () => {
+    taiDuLieuTongMainJs(() => console.info("Tai du lieu tong o mainjs"));
+  });
+}
