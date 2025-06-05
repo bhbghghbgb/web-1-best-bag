c3c9552b303c20c16367b07ef8b7ba340d06209b
bhbghghbgb <113711814+bhbghghbgb@users.noreply.github.com>
2025-06-06 02:55:19 +0700

    Refactor code structure for improved readability and maintainability

--- doancu/wheel/vite.config.js
+++ doancu/wheel/vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
+import legacy from "@vitejs/plugin-legacy";
 
 export default defineConfig({
   base: process.env.VITE_BASE_URL ?? "./",
+  plugins: [legacy()],
 });
